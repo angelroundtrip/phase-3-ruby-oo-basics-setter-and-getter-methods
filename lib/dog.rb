02415f78a7264911dde3f83a671c19ea1029b423
Angel Rosario <arosario.us@gmail.com>
2022-02-23 11:24:31 -0500

    commit tree 8a81cf9d9b64b853724ed0254c4510fce29162f6

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
+  
+  #! getter
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  #!setter
+  def name 
+    @name
+  end
+
+  #! getter
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  #!setter
+  def breed 
+    @breed
+  end
 
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,23 @@
 class Person
 
+  #! getter
+  def name=(person_name)
+    @name = person_name
+  end
+
+  #!setter
+  def name 
+    @name
+  end
+
+  #! getter
+  def job=(job_type)
+    @job = job_type
+  end
+
+  #!setter
+  def job 
+    @job
+  end
+
 end
